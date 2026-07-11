5e48538e759e40659bfabf7ac171e01cd205d44f
lance.guinto <lance.guinto@pragmaticlabs.com.au>
2026-07-11 17:51:55 +0800

    Added payer_name to expenses_controller.db

--- backend/app/controllers/api/expenses_controller.rb
+++ backend/app/controllers/api/expenses_controller.rb
@@ -54,6 +54,7 @@ class Api::ExpensesController < ApplicationController
       amount: expense.amount.to_f,
       category: expense.category.name,
       date: expense.date.to_s,
+	  payer_name: expense.payer_name,
       created_at: expense.created_at,
       updated_at: expense.updated_at
     }
